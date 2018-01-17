41b5ae54de3cd84a9c627b1a2c44778ad7a48234
Patrik Šimunič <patriksimunic@hotmail.com>
2018-01-17 14:06:07 +0100

    CROS

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var httpProxy = createProxyServer({
     target: 'http://suggestqueries.google.com',
 })
 
+httpProxy.on('proxyRes', (proxyRes, req, res) => {
+    res.setHeader('Access-Control-Allow-Headers', 'OPTIONS, GET')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+})
+
+app.options('*', function (req, res, next) {
+    res.status(200)
+    res.json({ status: 'ok' })
+})
+
 app.get('/complete/search', function (req, res, next) {
     httpProxy.web(req, res)
 })
